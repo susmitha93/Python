6040a296478f260cfc2571b954393cfe03a870f9
susmitha chimata <32884372+susmitha93@users.noreply.github.com>
2019-03-17 14:19:08 -0400

    sc

--- ProgramArr.py
+++ ProgramArr.py
@@ -1,7 +1,7 @@
 from array import array
 array_num=array('i',[1,3,5,7])
-for i in array_num:
- print(i)
+#for i in array_num:
+ #print(i)
 print(array_num[0])
 print(array_num[1])
 print(array_num[2])
