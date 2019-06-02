3091ce2da1f6e4e0cf239f7d74864acb583e5ba4
Chimata <susmitha.chimata@cotiviti.com>
2019-06-02 01:31:27 -0400

    sc

--- SumtoTarget.py
+++ SumtoTarget.py
@@ -1,18 +1,16 @@
 def twoSum(nums, target):
   num=sorted(nums)
   bol=False
-  for i in range(len(nums)-1,0,-1):
-    for j in range(len(nums)-2,-1,-1):
+  for i in range(len(num)-1,0,-1):
+    for j in range(len(num)-2,-1,-1):
       sum=num[i]+num[j];
       if (sum==target):
         bol=True
-        return nums.index(num[j]),nums.index(num[i])
+        return nums.index(num[i]),nums.index(num[j])
   if bol==False:
-    return "No pairs"
-       
-  
+    return []
 
-print twoSum([60,40,10,20,90],100)
+print twoSum([180,40,110,20,90],200)
 
 
 
