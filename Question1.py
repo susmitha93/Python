5b494583ad6f33efb7b1197ae08dd01397812122
susmitha chimata <32884372+susmitha93@users.noreply.github.com>
2019-07-27 16:36:35 -0400

    returns number of moves forming the work balloon in each move

---
+++ Question1.py
@@ -0,0 +1,26 @@
+def solution(S):
+    k='BALLOON'
+    count={}
+    for i in k:
+        cout=0
+        for j in S:
+            if j==i:
+                cout=cout+1
+        count[i]=cout
+    return validate(count)
+
+def validate(count):
+    minlo=min(count['L'],count['O'])
+    minlomoves=int(minlo)/2
+    minallothers=min(count['B'],count['A'],count['N'])
+    if minlomoves<minallothers:
+        return minlomoves
+    elif minlomoves<minallothers:
+        return minallothers
+    else:
+        return minallothers
+        
+print(solution('BALLONO'))
+
+
+        
